e3a0d245725c790400540f1743d59c4b59a5024e
Alex Sanchez <alesanchez728@gmail.com>
2018-03-03 09:54:52 -0600

    string of 0 returns int of 0

---
+++ StringCalculator.App/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.App
+{
+    public class Converter
+    {
+        //Create a simple String calculator with a method int Add(string numbers)
+        public int Add(string numbers)
+        {
+            var result = 0;
+            var numberCount = numbers.Length;
+
+            if (numberCount == 0)
+            {
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using StringCalculator.App;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringConverterTests
+    {
+        [Test]
+        public void Empty_string_to_converter_should_return_blank_string()
+        {
+            var stringOfNumbers = " ";
+            var stringConverter = new Converter();
+
+            var result = stringConverter.Add(stringOfNumbers);
+
+            Assert.AreEqual("0", result);
+        }
+    }
+}
